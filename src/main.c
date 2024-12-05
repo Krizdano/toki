7d571cb7ccf30ba036d46d65f5658d0b50dfa1d0
krizdano <opensauce.passover167@slmail.me>
2024-12-05 15:07:11 +0530

    main.c: added comments

--- src/main.c
+++ src/main.c
@@ -7,11 +7,30 @@
 #include "commands/inc/help-command.h"
 #include "commands/inc/options.h"
 
-
+/*
+ * struct Args - Structure to hold command names and their corresponding function pointers.
+ * @name: Command name.
+ * @function: Function pointer to execute the command.
+ *
+ * This structure associates a command name (as a string) with a function that
+ * executes the command. The function takes an array of command-line arguments.
+ */
 typedef struct {
   const char *name;
   void (*function)(const char *argv[]);
 } Args;
+
+/*
+ * main - Main entry point of the command-line application.
+ * @argc - The number of command-line arguments.
+ * @argv - The array of command-line arguments.
+ *
+ * This function processes command-line arguments, checks for valid commands,
+ * and invokes the corresponding functions. If no valid command is provided,
+ * it displays usage information.
+ *
+ * Return: Exit status: 0 on success, 1 if no command is provided, or 2 if an invalid command is given.
+*/
 
 int main(int argc, char *argv[]) {
 
@@ -20,6 +39,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  /* Check for any command-line options and adjust argc and argv accordingly */
   CheckForOptions(&argc, argv);
 
   Args commands[] = {
@@ -34,10 +54,11 @@ int main(int argc, char *argv[]) {
   size_t number_of_commands = sizeof(commands)/sizeof(commands[0]);
   for(size_t i = 0; i < number_of_commands; i++) {
     if (strcmp(argv[1], commands[i].name) == 0) {
-      commands[i].function( (const char**) argv);
+      commands[i].function( (const char**) argv);  /* Execute the command */
       return 0;
     }
   }
+  /* If no valid command is found, notify the user and display usage information */
   printf("%s is not a valid command\n", argv[1]);
   usage();
   return 2;
